06ab7944afab0a7c24f321eb6ba468feeaade324
erberg <berg.ericr@gmail.com>
2013-04-29 19:40:13 -0400

    Fixed HTML to Validate

--- application/views/about/index.blade.php
+++ application/views/about/index.blade.php
@@ -18,12 +18,12 @@
 
 <div class="container article-container">
 	<div class="eight columns">
-			<p>I work hard to make the Internet better. Whether front or back-end programming, it's my passion to create great web applications using the latest standards and technology. My dedication to self-learning makes me well-suited to this quickly evolving field, and my formal education in engineering and art combine to give me an expert eye for both function and aesthetics. </p>
+			<p>I work hard to make the internet better. Whether front or back-end programming, it's my passion to create great web applications using the latest standards and technology. My dedication to self-learning makes me well-suited to this quickly evolving field, and my formal education in engineering and art combine to give me an expert eye for both function and aesthetics. </p>
 			<p>When I'm not researching new and exciting technologies, I like to spend my free time exploring nature, going to concerts, or creating hideous noises with my guitar.</p>
 			<p>If you'd like to work with me, or learn more about what I do, just <a href="{{URL::to_route('contact')}}">let me know</a>.</p>
 	</div>
 
-	<div class="five columns"><div style='float:right;'><img src="img/eric.jpg" width="100%px"/></div></div>
+	<div class="five columns"><div style='float:right;'><img src="img/eric.jpg" class="scale-with-grid" alt="A picture of Eric Berg."/></div></div>
 
 </div><!-- container -->
 
